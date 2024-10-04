f2fd10f79db99553f2ddf9fde78e35e4b2e38526
Ashish <ashish763shukla@gmail.com>
2024-10-04 15:15:34 +0530

    Mentioned credentials path

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                   = "eu-west-1"
-  shared_credentials_files = ["C:/Users/ashis/.aws/credentials"]
+  shared_credentials_files = ["C:/Users/ashis/.aws/credentials"]     # Path to AWS credentials file
 }
